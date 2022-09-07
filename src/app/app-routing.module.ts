3ea2747af1f1aa5dbeb6dd15ad9fa28aa1b93e16
smart-think-app <79594145+smart-think-app@users.noreply.github.com>
2022-09-07 12:18:13 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
-  // }
+  {
+    path: '',
+    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
+  }
 ];
 
 @NgModule({
